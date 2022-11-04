77c7acb95896b98a64f96085a5c17e845f77b8d9
Larissa <larissamf25@gmail.com>
2022-11-04 18:38:58 -0300

    feat: req 7

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -28,7 +28,35 @@ class UserController {
     res.status(statusCodes.OK).json({ token });
   };
 
+  public validateUser = (body: object) => {
+    const schema1 = Joi.object({
+      username: Joi.required(),
+      classe: Joi.required(),
+      level: Joi.required(),
+      password: Joi.required(),
+    });
+    const { error: error1 } = schema1.validate(body);
+    if (error1) return ['1', error1.message];
+    const schema2 = Joi.object({
+      username: Joi.string().min(3).required(),
+      classe: Joi.string().min(3).required(),
+      level: Joi.number().min(1),
+      password: Joi.string().min(8),
+    });
+    const { error: error2 } = schema2.validate(body);
+    if (error2) return ['2', error2.message];
+    return ['0', 'ok'];
+  };
+
   public createUser = async (req: Request, res: Response) => {
+    const [type, message] = this.validateUser(req.body);
+    if (type === '1') {
+      return res.status(statusCodes.BAD_REQUEST).json({ message });
+    }
+    if (type === '2') {
+      return res.status(statusCodes.INCORRECT).json({ message });
+    }
+
     const { username, classe, level, password } = req.body;
     const token = await this.userService.createUser(username, classe, level, password);
     res.status(statusCodes.CREATED).json({ token });
